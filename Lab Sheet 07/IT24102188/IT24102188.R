ee1c8181a577db69f2de7dea13b409b34ea77cf3
IT24102188 <IT24102188@my.sliit.lk>
2025-09-16 14:15:24 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102188/IT24102188.R
@@ -0,0 +1,19 @@
+setwd("C:\\Users\\ASUS\\Desktop\\IT24102188")
+
+
+#Question 1
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+
+
+#Question 2
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+
+#Question 3.1
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+
+#Question 3.2
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
